944017e8cf2a9ced2ec7c1d2379515ce8fb1bdcf
Confundens <omer.olkun@hotmail.com>
2020-01-14 21:56:16 +0300

    aaaaaaaaaaaaa
    bbbbbbbbbbbbbbbbbb

---
+++ Assets/Scripts/EssentialsLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EssentialsLoader : MonoBehaviour
+{
+    
+    public GameObject UIScreen;
+    public GameObject player;
+    public GameObject gameMan;
+    void Start()
+    {
+        if (IUFade.instance == null)
+        {
+            IUFade.instance = Instantiate(UIScreen).GetComponent<IUFade>();
+        }
+
+        if (PlayerContoller.instance == null)
+        {
+            PlayerContoller clone = Instantiate(player).GetComponent<PlayerContoller>();
+            PlayerContoller.instance = clone;
+        }
+
+        if(GameManager.instance==null)
+        {
+            Instantiate(gameMan);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
